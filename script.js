42f6284c9bddf173f8a0ab1197a21c95063ccf6b
Muhammad Adnan <adnanqasmi267@gmail.com>
2024-12-25 16:27:08 +0000

    updated

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ function addTask() {
     const taskInput = document.getElementById('todo-input');
     const taskText = taskInput.value.trim();
 
-    if (taskText === '') 
-        alert("Please Enter Your Task");
+    if (taskText === '') return;
         
 
     const listItem = document.createElement('li');
